7026b07c1712fe006764a750fac9baf059daabc9
Naohisa Takahashi <hakuro16@gmail.com>
2014-05-11 23:54:20 +0900

    initial commit

---
+++ src/leprechaun/routes/home.clj
@@ -0,0 +1,15 @@
+(ns leprechaun.routes.home
+  (:use compojure.core)
+  (:require [leprechaun.views.layout :as layout]
+            [leprechaun.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
